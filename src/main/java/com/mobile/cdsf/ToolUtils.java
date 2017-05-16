12a239dacbe49d3c1d23abc0c0fa9583cd59b77a
吴文亮 <wuwenliang@sc.10086.cn>
2017-05-16 22:41:08 +0800

    TestModule

---
+++ src/main/java/com/mobile/cdsf/ToolUtils.java
@@ -0,0 +1,11 @@
+package com.mobile.cdsf;
+
+/**
+ * Created by wuwenliang on 2017/5/16.
+ */
+
+public class ToolUtils {
+    public static void showLog(){
+        System.out.println("--wuwenliangLLLPP:::___ToolUtils");
+    }
+}
